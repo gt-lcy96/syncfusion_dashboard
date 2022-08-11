9fbd1c0a5041d461b9e9713959682499e07f6417
lohcy <loh.cheeyung96@gmail.com>
2022-08-11 13:41:00 +0800

    populate data to GridComponent

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   GridComponent,
   ColumnsDirective,
+  ColumnDirective,
   Resize,
   Sort,
   ContextMenu,
@@ -13,13 +14,20 @@ import {
   Inject
 } from '@syncfusion/ej2-react-grids';
 
-import { orderData, contextMenuItems, orderGrid } from '../data/dummy';
+import { ordersData, contextMenuItems, orderGrid, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
+      <GridComponent id="gridcomp" dataSource={ordersData}>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   );
 };

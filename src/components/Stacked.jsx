d23e238a5acc2f94e6df243a3dab9d669db75193
lohcy <loh.cheeyung96@gmail.com>
2022-08-11 13:27:23 +0800

    create Stacked chart

--- src/components/Stacked.jsx
+++ src/components/Stacked.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
 
-import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../data/dummy'
+import { stackedChartData, stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../data/dummy'
 
 const Stacked = ({width, height}) => {
   return (
     <ChartComponent
         width={width}
         height={height}
-        id="stack charts"
-        // primaryXAxis={stackedPrimaryXAxis}
-        // primaryYAxis={stackedPrimaryYAxis}
+        id="stack chart"
+        primaryXAxis={stackedPrimaryXAxis}
+        primaryYAxis={stackedPrimaryYAxis}
         chartArea={{ border: { width: 0 } }}
         tooltip={{  enable: true }}
         LegendSettings={{  background: 'white' }}
     >
         <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
         <SeriesCollectionDirective>
-            {stackedCustomSeries.map((item, index) => {
-                <SeriesDirective key={index} {...item}
-                />
-            })}
+        { 
+            stackedCustomSeries.map((item, index) => 
+            <SeriesDirective key={index} {...item}/>
+         )
+        }
+          {/* <SeriesDirective dataSource={stackedChartData[0]} xName='x' yName='y' width={2} name='India' type='StackingColumn'></SeriesDirective>
+          <SeriesDirective dataSource={stackedChartData[1]} xName='x' yName='y1' width={2} name='China' type='StackingColumn'></SeriesDirective>   */}
         </SeriesCollectionDirective>
     </ChartComponent>
   )

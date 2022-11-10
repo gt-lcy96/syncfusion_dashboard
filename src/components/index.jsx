4b6cc061bead3f4110f074da0d31d7218c6a172a
lohcy <loh.cheeyung96@gmail.com>
2022-11-10 15:07:13 +0800

    create LineChart

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -11,3 +11,4 @@ export { default as Header } from './Header';
 export { default as Chat } from './Chat';
 export { default as SparkLine} from './SparkLine';
 export { default as Stacked} from './Stacked';
+export { default as LineChart} from './LineChart';
